a526f1f8449023c15b2ab34e643d3e0d8b452522
Unai Romero Aldaz <unai.romero.aldaz@accenture.com>
2020-02-22 01:23:28 +0100

    C04 Override Statistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -38,5 +38,17 @@ namespace GradeBook.GradeBooks
 
             return 'F';
         }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+
+            base.CalculateStatistics();
+            return;
+        }
     }
 }
