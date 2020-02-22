24778ad7e12386c61bfd81a81d9ac3f4f950a55a
Unai Romero Aldaz <unai.romero.aldaz@accenture.com>
2020-02-22 01:16:56 +0100

    C03 Override RankedGradeBook's GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -7,9 +7,36 @@ namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
-        public RankedGradeBook(string name) : base(name)      
+        public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (averageGrade >= 80)
+            {
+                return 'A';
+            }
+            else if (averageGrade < 80 && (averageGrade >= 60))
+            {
+                return 'B';
+            }
+            else if (averageGrade < 60 && (averageGrade >= 40))
+            {
+                return 'C';
+            }
+            else if (averageGrade < 40 && (averageGrade >= 20))
+            {
+                return 'D';
+            }
+
+            return 'F';
         }
     }
 }
